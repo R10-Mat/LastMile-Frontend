9c9401c5782fa1149ed66953ae815c01d1304e0f
R10-Mat <rayhan.matos@utec.edu.pe>
2026-05-05 23:56:10 -0500

    Update eventosApi.js

--- src/api/eventosApi.js
+++ src/api/eventosApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const EVENTOS = axios.create({
-  baseURL: import.meta.env.VITE_EVENTOS_URL || "http://localhost:8001"
+  baseURL: import.meta.env.VITE_EVENTOS_URL || "http://localhost:3000"
 });
 
 export const getEventosByPedido = (pedidoId) =>
